c213542875ec158b82b6aabb21a27fe6a1ce9522
JustCodiex <codiex1944@gmail.com>
2023-03-29 20:33:56 +0200

    Added testing

---
+++ CoH2XML2JSON-Test/Strategy/CoH2/CoH2SlotItemReaderTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CoH2XML2JSON.Blueprints;
+using CoH2XML2JSON.Strategy;
+using CoH2XML2JSON.Strategy.CoH2;
+
+using CoH2XML2JSON_Test.Xml;
+
+namespace CoH2XML2JSON_Test.Strategy.CoH2;
+
+public class CoH2SlotItemReaderTest {
+
+    [NotNull]
+    private XmlResource slotItemXmlData;
+
+    [NotNull]
+    private CoH2SlotItemReader slotItemReader;
+
+    [SetUp]
+    public void Setup() {
+        slotItemReader = new CoH2SlotItemReader();
+        slotItemXmlData = new XmlResource("SlotItemDummy.xml");
+    }
+
+    [Test]
+    public void CanParseCoH2SlotItem() {
+
+        // Load
+        SlotItemBlueprint sib = slotItemReader.FromXml(slotItemXmlData.Document, string.Empty, "SlotItemDummy.xml", new Helpers());
+        Assert.That(sib, Is.Not.Null);
+        Assert.That(sib.SlotSize, Is.EqualTo(6));
+
+    }
+
+}
---
+++ CoH2XML2JSON-Test/Xml/XmlResource.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml;
+
+namespace CoH2XML2JSON_Test.Xml;
+
+public class XmlResource {
+
+    private XmlDocument xmlDocument;
+
+    public XmlDocument Document => xmlDocument;
+
+    public XmlResource(string resourcename) {
+        xmlDocument = new XmlDocument();
+        xmlDocument.LoadXml(File.ReadAllText("Xml/" + resourcename));
+    }
+
+}
